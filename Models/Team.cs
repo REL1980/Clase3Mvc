c7f89c3a7551ef4e32607bf5434173014c8d8c15
REL1980 <rafaelandeau@hotmail.com>
2022-01-04 18:41:47 -0300

    first commit

---
+++ Models/Team.cs
@@ -0,0 +1,13 @@
+namespace Clase3Mvc.Models;
+public class Team
+{
+    public Team(int id, string name)
+    {
+        Name = name;
+        Id = id;
+    }
+
+    public int Id { get; set; }
+
+    public string Name { get; set;}
+}
